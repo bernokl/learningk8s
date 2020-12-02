9b2c8c01fbddb12f9fd588c67da7d009c74d30ab
Berno Kleinhans <bb@ii.coop>
2020-12-02 15:45:04 +1300

    Playing with env vars

--- fortune-env/fortuneloop.sh
+++ fortune-env/fortuneloop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 trap "exit" SIGINT
+## comment out the next line to show this is the one removed 
+## INTERVAL=$1
 mkdir /var/htdocs
 while :
 do
+  echo Configured to generate new fortune every $INTERVAL seconds
   echo $(date) Writing fortune to /var/htdocs/index.html
   /usr/games/fortune > /var/htdocs/index.html
-  sleep 10
+  sleep $INTERVAL
 done
